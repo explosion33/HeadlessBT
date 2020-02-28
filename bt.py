7a826821997a2b427e3f663e5f0f56b72ee2e8ad
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-02-27 20:56:36 -0800

    added a bt.py script that has functions to handle bluetooth bt.py can also act as a pairing request manager

---
+++ bt.py
@@ -0,0 +1,112 @@
+import subprocess
+
+def runCmd(cmd):
+    """
+    runCmd (cmd): runs a command in the shell (can use a heredoc)\n
+    cmd : str containing a command\n
+    returns : the console output from running the command
+    """
+    x = subprocess.check_output(cmd, shell=True)
+    x = x.decode("utf-8")
+    return x
+
+def getDevices():
+    """
+    getDevices () : gets a dictionary of all connected devices and their mac address\n
+    returns : {"name": "MAC",}
+    """
+
+    cmd = '''bluetoothctl <<EOF
+    devices
+    exit
+    EOF
+    '''
+    out = repr(runCmd(cmd))
+    devices = {}
+
+    while out.find("Device") != -1:
+        x = out.find("Device")
+        MAC = out[x+7:x+7+17]
+
+        name = ""
+        for i in range(len(out)-x-25):
+            p = out[i+x+25]
+            if p + out[i+x+26] == "\\n":
+                break
+            name += p
+        devices[name] = MAC
+        out = out[x+7+16::]
+
+    return devices
+
+def getNewDevices(deviceList):
+    """
+    getNewDevices(deviceList) : gets the devices that have been added since the previous list\n
+    deviceList : a list obtained through getDevices that is older by atleast a tick\n
+    returns : {"name": "MAC",}
+    """
+    d = getDevices()
+
+    devices = {}
+
+    if d != deviceList:
+        dK = list(d.keys())
+        dK2 = list(deviceList.keys())
+
+        for key in dK:
+            if key not in dK2:
+                devices[key] = d[key]
+
+    return devices
+
+def pair(device):
+    """
+    remove(device) : pairs a device by Trusting it\n
+    device : a string containing the mac address of the device\n
+    returns : console log during command
+    """
+
+    cmd1 = '''bluetoothctl <<EOF
+    trust '''
+
+    cmd2 = '''
+    exit
+    EOF
+    '''
+
+    cmd = cmd1 + str(device) + cmd2
+    out = repr(runCmd(cmd))
+    return out
+
+def remove(device):
+    """
+    remove(device) : removes a device from the paired bluetooth devices\n
+    device : a string containing the mac address of the device\n
+    returns : console log during command
+    """
+    cmd1 = '''bluetoothctl <<EOF
+    remove '''
+
+    cmd2 = '''
+    exit
+    EOF
+    '''
+
+    cmd = cmd1 + str(device) + cmd2
+    out = repr(runCmd(cmd))
+
+    return out
+
+if "main" in __name__: #if bt is called directly it runs a sctipt to handle incoming pairing requests
+    devices = getDevices()
+    print(devices)
+    while True:
+        x = getNewDevices(devices)
+        if x:
+            print(x)
+            k = list(x.keys())
+            out = pair(x[k[0]])
+            print(out)
+            break
+        else:
+            print(x, devices)
