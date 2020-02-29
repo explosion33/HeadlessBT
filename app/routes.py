5e2efb3076d76581732e519d57c757037a0511a9
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-02-28 16:34:18 -0800

    added bluetooth page and displaying of device names

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from flask import render_template, flash, redirect, request, url_for
 from app import app
 import os
+import bt
+
+devices = bt.getDevices()
 
 #main page plus random key handles transfers
 @app.route('/')
 def index():
     return render_template("home.html")
+
+@app.route('/bt')
+def blueTooth():
+    global devices
+    devics = bt.getDevices()
+
+    d = list(devices.keys())
+
+    return render_template("bluetooth.html", devives=d)
