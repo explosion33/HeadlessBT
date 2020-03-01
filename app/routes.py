795cc8d10328379bc99e315c11e229de4a837f00
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-03-01 10:23:15 -0800

    added a removal page and buttons that direct to it

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app
 import os
 import bt
 
-devices = bt.getDevices()
+#devices = bt.getDevices()
 
 #main page plus random key handles transfers
 @app.route('/')
@@ -11,13 +11,23 @@ def index():
     return render_template("home.html")
 
 @app.route('/bt')
-def blueTooth():
+def bluetooth():
     global devices
-    devices = bt.getDevices()
-    #devices = {'EthanPhone': 'F0:A3:5A:7B:A4:19', "Bill's IIIIphone": 'F0:A3:5A:7B:A4:19'} #fake data for Windows testing
-    c = bt.getConnected()
-    #c =  {'Name': 'EthanPhone', 'Type': 'phone'}
+    #devices = bt.getDevices()
+    devices = {'EthanPhone': 'F0:A3:5A:7B:A4:19', "Bill's IIIIphone": 'F0:A3:5A:7B:A4:19'} #fake data for Windows testing
+    #c = bt.getConnected()
+    c =  {'Name': 'EthanPhone', 'Type': 'phone'}
 
     d = list(devices.keys())
 
     return render_template("bluetooth.html", devices=d, connected=c)
+@app.route('/remove/<device>')
+def remove(device):
+    global devices
+
+    print("removing", device)
+
+    MAC = devices[device]
+    bt.remove(MAC)
+
+    return redirect(url_for("bt"))
