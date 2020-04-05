4924b04a36a16a785f4b069cd6276f404b25f6b6
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-04-04 23:33:27 -0700

    updated auto.py to announce start and conenction

--- auto.py
+++ auto.py
@@ -1,5 +1,8 @@
 import bt
 import time
+from playsound import playsound
+
+playsound('app/static/audio/init.mp3')
 
 start = time.time()
 devices = bt.getDevices()
@@ -7,5 +10,6 @@ while time.time() - start < 15:
     for device in list(devices.keys()):
         bt.connect(devices[device])
     if bt.getConnected():
+        playsound('app/static/audio/connected.mp3')
         break
 bt.advertise()
