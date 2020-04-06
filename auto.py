14690c476b9b5910d71c1d26902bb335b3d3a0c8
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-04-05 21:19:33 -0700

    f

--- auto.py
+++ auto.py
@@ -7,20 +7,26 @@ pg.init()
 pg.mixer.init()
 
 def play(file):
-    pg.mixer.load(file)
-    pg.mixer.play()
+    pg.mixer.music.load(file)
+    pg.mixer.music.play()
     pg.event.wait()
 
-print(sys.argv)
 
-play('app/static/audio/init.mp3')
+root = sys.argv[1]
+
+play(root + '/app/static/audio/init.mp3')
 
 start = time.time()
 devices = bt.getDevices()
-while time.time() - start < 15:
-    for device in list(devices.keys()):
-        bt.connect(devices[device])
-    if bt.getConnected():
-        play('app/static/audio/connected.mp3')
-        break
-bt.advertise()
+while True:
+    while time.time() - start < 15:
+        print("searching")
+        for device in list(devices.keys()):
+            bt.connect(devices[device])
+        if bt.getConnected():
+            play(root + '/app/static/audio/connected.mp3')
+            break
+    bt.advertise()
+    start = time.time()
+    while not bt.getConnected():
+        print("waiting")
